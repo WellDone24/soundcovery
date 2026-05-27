2742a5a048ca498011359676fdd5802179ef6a0e
Heinrich Burlage <heinrich.burlage@welldonedecisions.com>
2026-05-27 10:19:45 +0200

    db update

--- app/qr/page.tsx
+++ app/qr/page.tsx
@@ -1,5 +1,5 @@
 import { redirect } from "next/navigation";
 
 export default function QRReferralPage() {
-  redirect("/?utm_source=festival_qr&utm_campaign=rock_for_people_2026");
+  redirect("/?utm_source=festival_qr");
 }
