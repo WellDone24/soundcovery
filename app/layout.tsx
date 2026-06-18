315e9db4679eaffee42d7c28e785559925869494
Heinrich Burlage <heinrich.burlage@welldonedecisions.com>
2026-06-18 11:58:43 +0200

    logo and layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,8 +5,40 @@ import "@fontsource/host-grotesk/700.css";
 import "./globals.css";
 
 export const metadata: Metadata = {
+  metadataBase: new URL("https://soundcovery.com"),
+
   title: "Soundcovery",
   description: "Find the artists you shouldn't miss",
+
+  openGraph: {
+    title: "Soundcovery",
+    description: "Find the artists you shouldn't miss",
+    url: "https://soundcovery.com",
+    siteName: "Soundcovery",
+    locale: "en_US",
+    type: "website",
+    images: [
+      {
+        url: "/og-image.png",
+        width: 1200,
+        height: 630,
+        alt: "Soundcovery",
+      },
+    ],
+  },
+
+  twitter: {
+    card: "summary_large_image",
+    title: "Soundcovery",
+    description: "Find the artists you shouldn't miss",
+    images: ["/og-image.png"],
+  },
+
+  icons: {
+    icon: "/favicon.ico",
+    shortcut: "/favicon.ico",
+    apple: "/favicon.ico",
+  },
 };
 
 export default function RootLayout({
